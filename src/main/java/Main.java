8c69899eb59277cdfb259944d4426d16c697a863
nekit <nikitakosare1@list.ru>
2026-09-02 01:49:00 +0500

    one commite

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("r");
+
+    }
+}
